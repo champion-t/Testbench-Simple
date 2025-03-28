ad0e09110f9d07d3744dd0fd75a64e828001421a
champion-t <thanhapolo11@gmail.com>
2025-03-28 15:08:40 +0700

    Add package.sv & filelist.f

---
+++ reg wr simple/package.sv
@@ -0,0 +1,8 @@
+package my_pkg;
+    `include "packet.sv"
+    `include "driver.sv"
+    `include "monitor.sv"
+    `include "scoreboard.sv"
+    `include "environment.sv"
+    `include "test.sv"
+endpackage
